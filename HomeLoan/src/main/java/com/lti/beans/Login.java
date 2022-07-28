0b461874110761b5b0bdd77d7cdcb569b20d4a08
gayatriuh <gayatriumesh34@gmail.com>
2022-07-28 10:56:36 +0530

    1st commit on branch gayatri

--- HomeLoan/src/main/java/com/lti/beans/Login.java
+++ HomeLoan/src/main/java/com/lti/beans/Login.java
@@ -1,22 +1,102 @@
 package com.lti.beans;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+@Entity
+@Table(name = "LOGIN")
 public class Login {
 	
+	
 	@Id
-	@Column(name = "user_id")
-//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOGIN_GEN")
-//	@SequenceGenerator(name = "LOGIN_GEN", sequenceName = "login_id_seq")
-	private Integer userId;
-	
-	
+	@Column(name="USERNAME")
 	private String username;
 	
+	@Column(name="PASSWORD")
 	private String password;
 	
+	@Column(name="ROLE")
 	private String role;
+
+
+	
+	public Login(String username, String password, String role) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+
+
+
+
+
+	public Login() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+
+
+
+	@Override
+	public String toString() {
+		return "Login [username=" + username + ", password=" + password + ", role=" + role + "]";
+	}
+	
+	
 	
 	
 
