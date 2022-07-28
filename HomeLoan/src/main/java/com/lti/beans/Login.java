98c12078cb91b99fd403865e1eac3933d73c153e
gayatriuh <gayatriumesh34@gmail.com>
2022-07-28 10:47:03 +0530

    4th commit

--- HomeLoan/src/main/java/com/lti/beans/Login.java
+++ HomeLoan/src/main/java/com/lti/beans/Login.java
@@ -1,5 +1,24 @@
 package com.lti.beans;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 public class Login {
+	
+	@Id
+	@Column(name = "user_id")
+//	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOGIN_GEN")
+//	@SequenceGenerator(name = "LOGIN_GEN", sequenceName = "login_id_seq")
+	private Integer userId;
+	
+	
+	private String username;
+	
+	private String password;
+	
+	private String role;
+	
+	
+
 
 }
